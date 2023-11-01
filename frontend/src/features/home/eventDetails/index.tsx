ae6df91d9e64c4993ebd003a9d62516e124ff91c
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2023-11-01 20:52:24 -0300

    feat: add event details

--- frontend/src/features/home/eventDetails/index.tsx
+++ frontend/src/features/home/eventDetails/index.tsx
@@ -1,9 +1,23 @@
 import { Stack } from "@/components/stack";
+import { Text } from "@/components/text";
+
+import styles from './index.module.css'
 
 export function EventDetails() {
   return (
-    <Stack full style={{ background: '#ccc' }}>
-      <div >detalhes</div>
+    <Stack padding="md" full className={styles.container}>
+      <Stack gap="sm">
+        <Text size="lg">
+          Detalhes do churras
+        </Text>
+
+
+        <Stack direction="row" align="center" gap="sm">
+          <Text variant='h5' color="secondary">Churras{' > '}</Text>
+
+          <Text variant='h5' color="secondary">Nome do churras</Text>
+        </Stack>
+      </Stack>
     </Stack>
   )
 }
