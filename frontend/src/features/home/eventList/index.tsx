6a680d01c2df915092c42f270f2ffb49d52b780c
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2023-11-01 18:04:20 -0300

    styles: text field

--- frontend/src/features/home/eventList/index.tsx
+++ frontend/src/features/home/eventList/index.tsx
@@ -3,6 +3,7 @@ import { Stack } from "@/components/stack";
 import styles from './index.module.css'
 import { Text } from "@/components/text";
 import { IconButton } from "@/components/iconButton";
+import { TextField } from "@/components/form/textField";
 
 export function EventList() {
   return (
@@ -12,12 +13,20 @@ export function EventList() {
     >
       <Stack padding="sm" direction="row" align="center" justify="space-between">
         <Text size="lg" ml="md">
-          Churras
+          Todos os churras
         </Text>
 
         <IconButton
           name="add"
           variant="sm"
+        />
+      </Stack>
+
+      <Stack padding="sm">
+        <TextField
+          label="Pesquisar"
+          name="search"
+          placeholder="Pesquisar"
         />
       </Stack>
 
