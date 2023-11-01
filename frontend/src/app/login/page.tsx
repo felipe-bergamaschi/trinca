2988ece256343af8eafad7974e273d29454631f0
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2023-11-01 11:53:49 -0300

    feat: add login page

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,14 @@
+import { AuthLayout } from '@/components/layout/auth'
+import { Login } from '@/features/login'
+import { MockLogin } from '@/features/login/mock'
+import { Suspense } from 'react'
+
+export default function Page() {
+  return (
+    <AuthLayout>
+      <Suspense fallback={<MockLogin />}>
+        <Login />
+      </Suspense>
+    </AuthLayout>
+  )
+}
