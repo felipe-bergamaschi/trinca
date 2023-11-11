a9952f353b9c5118335def331fc002b697f7d505
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2023-11-11 16:09:18 -0300

    feat: add box cp

---
+++ frontend/src/components/box/index.tsx
@@ -0,0 +1,29 @@
+import { Button } from "../button"
+import { Stack } from "../stack"
+
+import styles from './index.module.css'
+
+interface BoxProps {
+  title: string
+  children: React.ReactNode
+}
+
+export function Box({ title, children }: BoxProps) {
+  return (
+    <Stack className={styles.container}>
+      <Stack padding="md">
+        {title}
+      </Stack>
+
+      <Stack padding="md" gap="md">
+        {children}
+      </Stack>
+
+      <Stack padding="md" justify="end" direction="row" gap="md" className={styles.footer}>
+        <Button>
+          Confirmar
+        </Button>
+      </Stack>
+    </Stack>
+  )
+}
