1c332986ae5eac0243b0d0239d6ab5e2cbe92ad6
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2023-11-11 14:15:11 -0300

    feat: add container

---
+++ frontend/src/components/container/index.tsx
@@ -0,0 +1,34 @@
+import { Stack } from "../stack";
+import { Text } from "../text";
+
+import styles from './index.module.css';
+
+interface ContainerProps {
+  title: string;
+  breadcrumb: string[];
+  children: React.ReactNode;
+}
+
+export function Container({ title, breadcrumb, children }: ContainerProps) {
+  return (
+    <Stack padding="md" full className={styles.container}>
+      <Stack gap="md">
+        <Stack gap="sm">
+          <Text size="lg">
+            {title}
+          </Text>
+
+          <Stack direction="row" align="center" gap="sm">
+            <Text variant='h5' color="secondary">
+              {['Churras', ...breadcrumb].join(' > ')}
+            </Text>
+          </Stack>
+        </Stack>
+
+        <Stack>
+          {children}
+        </Stack>
+      </Stack>
+    </Stack>
+  )
+}
