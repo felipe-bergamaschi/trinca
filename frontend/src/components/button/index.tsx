d290da739b104526a8e9eab982239c3c7e9e3bb4
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2023-11-13 14:53:57 -0300

    chore: event details

--- frontend/src/components/button/index.tsx
+++ frontend/src/components/button/index.tsx
@@ -4,13 +4,15 @@ interface ButtonProps {
   children: React.ReactNode;
   fullWidth?: boolean
   type?: HTMLButtonElement['type'];
+  onClick?: () => void;
 }
 
-export function Button({ children, fullWidth, type = 'button' }: ButtonProps) {
+export function Button({ children, fullWidth, type = 'button', onClick }: ButtonProps) {
   return (
     <button
       type={type}
       className={styles.button}
+      onClick={onClick}
       style={{ width: fullWidth ? '100%' : 'fit-content' }}
     >
       {children}
--- frontend/src/features/home/eventDetails/index.tsx
+++ frontend/src/features/home/eventDetails/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Stack } from "@/components/stack";
 import { Text } from "@/components/text";
 
@@ -5,30 +7,118 @@ import { TextField } from "@/components/form/textField";
 import { Container } from "@/components/container";
 import { Box } from "@/components/box";
 
+import { Formik } from "formik";
+import { Button } from "@/components/button";
+import { IconButton } from "@/components/iconButton";
+import { useState } from "react";
+
+interface FormValues {
+  date: string;
+  address: string;
+  description: string;
+  people: {
+    id: number
+    name: string;
+    amount: string;
+  }[];
+}
+
 export function EventDetails() {
+  const [people, setPeople] = useState<FormValues['people']>([])
+
+  function addPeople() {
+    setPeople([...people, { name: '', amount: '', id: Math.random() }])
+  }
+
+  function removePeople(id: number) {
+    setPeople(people.filter((item) => item.id !== id))
+  }
+
   return (
     <Container
       title="Novo Churras"
       breadcrumb={['Novo Churras']}
     >
-      <Box title="Preencha os dados">
-        <Stack direction="row" gap="md">
-          <TextField label="Data" name="asd" type="date" />
+      <Formik
+        initialValues={{} as FormValues}
+        onSubmit={(values, { setSubmitting }) => {
+          console.log({ values })
+          // setTimeout(() => {
+          //   alert(JSON.stringify(values, null, 2));
+          //   setSubmitting(false);
+          // }, 400);
+        }}
+      >
+        {({
+          handleSubmit,
+          values,
+          // errors,
+          // touched,
+          handleChange,
+          // handleBlur,
+          // isSubmitting,
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <Box title="Preencha os dados">
+              <Stack direction="row" gap="md">
+                <TextField
+                  label="Data"
+                  name="date"
+                  type="date"
+                  value={values.date}
+                  onChange={handleChange}
+                />
 
-          <TextField label="Endereço" name="asd" />
+                <TextField
+                  label="Endereço"
+                  name="address"
+                  value={values.address}
+                  onChange={handleChange}
+                />
 
-          <TextField label="Descrição" name="asd" />
-        </Stack>
+                <TextField
+                  label="Descrição"
+                  name="description"
+                  value={values.description}
+                  onChange={handleChange}
+                />
+              </Stack>
 
-        <Text>
-          Participantes do churras
-        </Text>
+              <Text>
+                Participantes do churras
+              </Text>
 
-        <Stack direction="row" gap="md">
-          <TextField label="Nome" name="asd" />
-          <TextField label="Telefone" name="asd" />
-        </Stack>
-      </Box>
+              <Button onClick={addPeople}>
+                Novo participante
+              </Button>
+
+              {people.map((item, index) => (
+                <Stack direction="row" gap="md" key={item.id}>
+                  <TextField
+                    label="Nome"
+                    name={`people[${index}].name`}
+                    value={values.people?.[index]?.name}
+                    onChange={handleChange}
+                  />
+
+                  <TextField
+                    label="Valor"
+                    name={`people[${index}].amount`}
+                    value={values.people?.[index]?.amount}
+                    onChange={handleChange}
+                  />
+
+                  <IconButton
+                    name="delete"
+                    onClick={() => removePeople(item.id)}
+                  />
+                </Stack>
+              ))}
+            </Box>
+          </form>
+        )}
+      </Formik>
+
     </Container>
   )
 }
