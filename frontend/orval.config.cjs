c749ac47031f009a21a6ac9eff448d05a90c8102
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2023-11-14 14:13:55 -0300

    feat: add orval config

---
+++ frontend/orval.config.cjs
@@ -0,0 +1,35 @@
+const path = require('path');
+
+/** @type {import('orval').Options} */
+module.exports = {
+  backend: {
+    output: {
+      mode: 'single',
+
+      client: 'react-query',
+
+      prettier: true,
+
+      target: path.resolve(__dirname, './src/query/index.ts'),
+
+      packageJson: require.resolve('./package.json'),
+
+      tsconfig: require.resolve('./tsconfig.json'),
+
+      override: {
+        mutator: {
+          path: require.resolve('./src/api/axios.ts'),
+          name: 'request'
+        },
+        query: {
+          useQuery: true,
+        },
+        header: () => '//@ts-nocheck\n'
+      }
+    },
+
+    input: {
+      target: require.resolve('../backend/swagger.json')
+    }
+  }
+};
