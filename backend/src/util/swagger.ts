a67f09b506c666b57ef92592b7174866fd273e68
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2023-11-14 14:16:32 -0300

    chore: move swagger

--- backend/src/util/swagger.ts
+++ backend/src/util/swagger.ts
@@ -4,12 +4,12 @@ export async function updateSwagger(swaggerObj: unknown) {
   const swagger = JSON.stringify(swaggerObj, null, 2);
 
   try {
-    const current = await readFile('./swagger.json', 'utf-8');
+    const current = await readFile('../frontend/swagger.json', 'utf-8');
 
     if (current === swagger) {
       return;
     }
   } catch { }
 
-  await writeFile('./swagger.json', swagger);
+  await writeFile('../frontend/swagger.json', swagger);
 }
