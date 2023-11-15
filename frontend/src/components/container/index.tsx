e9ba771bcc79dd2d72c2455887aa7b5ef6df9597
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2023-11-15 15:38:55 -0300

    chore: add on close

--- frontend/src/components/container/index.tsx
+++ frontend/src/components/container/index.tsx
@@ -1,3 +1,4 @@
+import { IconButton } from "../iconButton";
 import { Stack } from "../stack";
 import { Text } from "../text";
 
@@ -7,23 +8,33 @@ interface ContainerProps {
   title: string;
   breadcrumb: string[];
   children: React.ReactNode;
+  onClose?: () => void;
 }
 
-export function Container({ title, breadcrumb, children }: ContainerProps) {
+export function Container({ title, breadcrumb, children, onClose }: ContainerProps) {
   return (
     <Stack align="center" full className={styles.container}>
       <Stack full className={styles.content}>
         <Stack padding="md" gap="md" full>
-          <Stack gap="sm">
-            <Text size="lg">
-              {title}
-            </Text>
-
-            <Stack direction="row" align="center" gap="sm">
-              <Text variant='h5' color="secondary">
-                {['Churras', ...breadcrumb].join(' > ')}
+          <Stack direction="row" align="center" gap="md" justify="space-between">
+            <Stack gap="sm">
+              <Text size="lg">
+                {title}
               </Text>
+
+              <Stack direction="row" align="center" gap="sm">
+                <Text variant='h5' color="secondary">
+                  {['Churras', ...breadcrumb].join(' > ')}
+                </Text>
+              </Stack>
             </Stack>
+
+            {onClose && (
+              <IconButton
+                name="close"
+                onClick={onClose}
+              />
+            )}
           </Stack>
 
           <Stack gap="md" full>
