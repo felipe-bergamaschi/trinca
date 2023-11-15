8a05d38629072387325f4ce68dc26245fe05de2e
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2023-11-15 16:47:58 -0300

    chore: add hide icon

--- frontend/src/components/alert/index.tsx
+++ frontend/src/components/alert/index.tsx
@@ -7,21 +7,24 @@ import styles from './index.module.css'
 interface AlertProps {
   title: string
   children: React.ReactNode
+  hideIcon?: boolean
 }
 
-export function Alert({ title, children }: AlertProps) {
+export function Alert({ title, children, hideIcon = false }: AlertProps) {
   return (
     <Stack className={styles.container} padding="md" gap="md">
       <Stack direction="row" gap="md" align="center">
-        <Icon name="info" color="var(--text-secondary)" />
+        {!hideIcon && (
+          <Icon name="info" color="var(--text-secondary)" />
+        )}
 
-        <Text>
+        <Text color="secondary">
           {title}
         </Text>
       </Stack>
 
       <Stack>
-        <Text color="secondary">
+        <Text>
           {children}
         </Text>
       </Stack>
